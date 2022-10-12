3e5120efa9c935afd7523e928c5327a1897b4382
amoghmc <amoghmc@github.com>
2022-10-12 07:10:35 +0530

    add pwd

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
@@ -7,19 +9,30 @@ int main()
 {
     // Configure readline to auto-complete paths when the tab key is hit.
     rl_bind_key('\t', rl_complete);
-
+    char cwd[100];
+    printf("hello");
     while (1) {
         // Display prompt and read input
-        char* input = readline("prompt> ");
+        getcwd(cwd, sizeof(cwd));
+        char* input = readline("");
+
 
         // Check for EOF.
         if (!input)
+
             break;
 
         // Add input to readline history.
         add_history(input);
 
         // Do stuff...
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+            printf("%s ", cwd);
+        } else {
+            perror("getcwd() error");
+            //return 1;
+        }
+        
 
         // Free buffer that was allocated by readline
         free(input);
