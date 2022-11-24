4b82d349961eedd8ff9cc17e9f8d4b133245f55c
amoghmc <amoghmc@github.com>
2022-11-24 10:35:53 +0530

    rm msleep

--- main.c
+++ main.c
@@ -13,8 +13,6 @@
 
 char *print_prompt();
 
-int msleep(long msec);
-
 void executeCommand(char *command, char *VarList[]);
 
 int MAX_PATH = 1024;
@@ -71,7 +69,6 @@ int main() {
 				if (status != 0) {
 					printf("Error! Child exited with error code %d\n", WEXITSTATUS(status));
 //					sleep for 50 milliseconds
-//					msleep(50);
 				}
 			}
 		}
@@ -109,24 +106,4 @@ char *print_prompt() {
 	free(cwd);
 	free(host);
 	return buffer;
-}
-
-
-// Source: https://stackoverflow.com/questions/1157209/is-there-an-alternative-sleep-function-in-c-to-milliseconds
-int msleep(long msec) {
-	struct timespec ts;
-	int res;
-	if (msec < 0) {
-		errno = EINVAL;
-		return -1;
-	}
-
-	ts.tv_sec = msec / 1000;
-	ts.tv_nsec = (msec % 1000) * 1000000;
-
-	do {
-		res = nanosleep(&ts, &ts);
-	} while (res && errno == EINTR);
-
-	return res;
 }
